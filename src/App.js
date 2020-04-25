fb185c87b13b18a36caa5383fa3349b1591b0bcf
Chuck Black <CBlack@ADEV.LOCAL>
2020-04-25 12:54:29 -0500

    added intitial routes for hard coded, rudementary hats.  used withRouts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage'
 
+const Hatspage = ({match}) => {
+  console.log(match);
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route path='/hats' component={Hatspage} />
+        <Route path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
